ac802a883a37bd4ead989ed69504232e72c7ad4b
ozan isgor <ozanisgor11@gmail.com>
2023-10-30 10:41:52 +0300

    UPDATE: font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Oswald } from "next/font/google";
 import { ReactElement } from "react";
+
+const oswald = Oswald({
+  subsets: ["latin"],
+});
 
 interface CustomAppProps extends Omit<AppProps, "Component"> {
   Component: AppProps["Component"] & {
@@ -10,5 +15,9 @@ interface CustomAppProps extends Omit<AppProps, "Component"> {
 
 export default function App({ Component, pageProps }: CustomAppProps) {
   const getLayout = Component.getLayout || ((page) => page);
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <div className={`${oswald.className}`}>
+      {getLayout(<Component {...pageProps} />)}
+    </div>
+  );
 }
